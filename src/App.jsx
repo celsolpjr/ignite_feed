4fec0d3e1135c8526e3a263f51ff58f3527b2abc
celsojr <celso.8882011@gmail.com>
2024-07-23 12:57:47 -0300

    Create global css and header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post 
         author = "Celso Lopes" 
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint eos, quas magni possimus necessitatibus? Nihil rerum ad exercitationem, esse saepe ipsum soluta ducimus deleniti quaerat totam ratione recusandae culpa." 
+      />
+      <Post 
+        author="Junior"
+        content="Novo post"
       />
     </div>
   )
