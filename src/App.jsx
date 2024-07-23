041fae070ab57b69583d9c7d1d14b0774803d1eb
celsojr <celso.8882011@gmail.com>
2024-07-22 22:37:03 -0300

    create new react project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        author = "Celso Lopes" 
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint eos, quas magni possimus necessitatibus? Nihil rerum ad exercitationem, esse saepe ipsum soluta ducimus deleniti quaerat totam ratione recusandae culpa." 
+      />
+    </div>
+  )
+}
