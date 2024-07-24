c275089b9884e7f03d9a86cf91545703d8a591bc
celso <celso.8882011@gmail.com>
2024-07-23 22:24:44 -0300

    inicialize sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import { Header } from "./components/Header"
-import { Post } from "./Post"
+import { Header } from "./components/Header";
+import { Post } from "./Post";
 
-import './global.css'
+import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author = "Celso Lopes" 
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint eos, quas magni possimus necessitatibus? Nihil rerum ad exercitationem, esse saepe ipsum soluta ducimus deleniti quaerat totam ratione recusandae culpa." 
-      />
-      <Post 
-        author="Junior"
-        content="Novo post"
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author = "Celso Lopes" 
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sint eos, quas magni possimus necessitatibus? Nihil rerum ad exercitationem, esse saepe ipsum soluta ducimus deleniti quaerat totam ratione recusandae culpa." 
+          />
+          <Post 
+            author="Junior"
+            content="Novo post"
+          />
+        </main>
+      </div>
     </div>
   )
 }
